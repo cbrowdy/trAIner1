9faac37127acabd864ad50568d1ffa8d509ae97b
Owen Zhang <owenzhang76@gmail.com>
2020-05-02 16:24:11 -0600

    working on dynamic buttons

---
+++ home.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    let patient = document.getElementById("patient-button");
+    let doctor = document.getElementById("doctor-button");
+    let investor = document.getElementById("investor-button");
+    $( "#patient-button" ).trigger( "click" );
+});
+
+function clicked() {
+    
+}
