1416b61a1d9fdec42b2190467c11078dc082e5bb
Owen Zhang <owenzhang76@gmail.com>
2020-05-02 19:15:23 -0600

    finished different forms and buttons

--- home.js
+++ home.js
@@ -1,10 +1,34 @@
 $(document).ready(function() {
+
     let patient = document.getElementById("patient-button");
     let doctor = document.getElementById("doctor-button");
     let investor = document.getElementById("investor-button");
-    $( "#patient-button" ).trigger( "click" );
-});
 
-function clicked() {
+    document.getElementById("doctor-form").style.display="none";
+    document.getElementById("investor-form").style.display="none";
     
-}
+    patient.addEventListener("click", function() {
+        let message = document.getElementById("interest-message");
+        message.innerHTML = "Interested in signing up for our beta?";
+        document.getElementById("patient-form").style.display="flex";
+        document.getElementById("doctor-form").style.display="none";
+        document.getElementById("investor-form").style.display="none";
+    });
+
+    doctor.addEventListener("click", function() {
+        let message = document.getElementById("interest-message");
+        message.innerHTML = "Interested in joining our platform as a primary care doctor?";
+        document.getElementById("patient-form").style.display="none";
+        document.getElementById("investor-form").style.display="none";
+        document.getElementById("doctor-form").style.display="flex";
+    });
+
+    investor.addEventListener("click", function() {
+        let message = document.getElementById("interest-message");
+        message.innerHTML = "Interested in learning more about our future milestones?";
+        document.getElementById("investor-form").style.display="flex";
+        document.getElementById("patient-form").style.display="none";
+        document.getElementById("doctor-form").style.display="none";
+    });
+
+});
