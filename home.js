39fc3bb5313074d2c97868ae038dc11a9e9fb5b0
Owen Zhang <owenzhang76@gmail.com>
2020-05-05 00:42:19 -0600

    form finally looks good

--- home.js
+++ home.js
@@ -11,18 +11,18 @@ $(document).ready(function() {
         let message = document.getElementById("interest-message");
         let doctor = document.getElementById("doctor-button");
 
-        patient.style.backgroundColor="#315d9d";
+        patient.style.backgroundColor="black";
         patient.style.outline="none";
         patient.style.color="#f6f6f6";
         patient.style.borderColor="#f6f6f6";
 
         doctor.style.backgroundColor="#f6f6f6";
-        doctor.style.color="#1F2B37";
-        doctor.style.borderColor="#1F2B37";
+        doctor.style.color="black";
+        doctor.style.borderColor="black";
 
         investor.style.backgroundColor="#f6f6f6";
-        investor.style.color="#191f53";
-        investor.style.borderColor="#191f53";
+        investor.style.color="black";
+        investor.style.borderColor="black";
 
         message.innerHTML = "Interested in signing up for our beta? Fill out the form here.";
         document.getElementById("patient-form").style.display="flex";
@@ -35,18 +35,18 @@ $(document).ready(function() {
         let investor = document.getElementById("investor-button");
         let patient = document.getElementById("patient-button");
 
-        doctor.style.backgroundColor="#315d9d";
+        doctor.style.backgroundColor="black";
         doctor.style.outline="none";
         doctor.style.color="#f6f6f6";
         doctor.style.borderColor="#f6f6f6";
 
         patient.style.backgroundColor="#f6f6f6";
-        patient.style.color="#315d9d";
-        patient.style.borderColor="#315d9d";
+        patient.style.color="black";
+        patient.style.borderColor="black";
 
         investor.style.backgroundColor="#f6f6f6";
-        investor.style.color="#315d9d";
-        investor.style.borderColor="#315d9d";
+        investor.style.color="black";
+        investor.style.borderColor="black";
 
         message.innerHTML = "Interested in joining our platform as a primary care doctor? Sign up here.";
         document.getElementById("patient-form").style.display="none";
@@ -59,7 +59,7 @@ $(document).ready(function() {
         let doctor = document.getElementById("doctor-button");
         let patient = document.getElementById("patient-button");
 
-        investor.style.backgroundColor="#315d9d";
+        investor.style.backgroundColor="black";
         investor.style.outline="none";
         investor.style.color="#f6f6f6";
         investor.style.borderColor="#f6f6f6";
@@ -69,8 +69,8 @@ $(document).ready(function() {
         doctor.style.borderColor="#315d9d";
 
         patient.style.backgroundColor="#f6f6f6";
-        patient.style.color="#315d9d";
-        patient.style.borderColor="#315d9d";
+        patient.style.color="black";
+        patient.style.borderColor="black";
         
         message.innerHTML = "Interested in learning more about our future milestones? Schedule a meeting with us.";
         document.getElementById("investor-form").style.display="flex";
